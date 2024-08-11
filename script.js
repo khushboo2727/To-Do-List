f7aa2b1f336a7aa29a60257b806f98bdb611c278
Khushboo <Khushboogupta2727@gmail.com>
2024-08-11 16:32:03 +0530

    added basic css and javascript

--- script.js
+++ script.js
@@ -1,1 +1,24 @@
-console.log("Hello! from script.js");
+// console.log("Hello! from script.js");
+function addContant(){
+    const ul = document.getElementById("list-element")
+    let li = document.createElement('li');
+    let addItem  = document.querySelector('#add-text');
+    let liValue = addItem.value
+    let text = document.createTextNode(liValue);
+    let checkbox = document.createElement("input")
+    checkbox.setAttribute('type' , 'checkbox');
+    li.appendChild(checkbox);
+    li.appendChild(text);
+    ul.appendChild(li);
+    addItem.value = ""
+}
+
+function makeComplete(){
+    let checkBox = document.querySelector('.checkBox');
+    let checkboxParent = checkBox.parentElement;
+    checkboxParent.style = 'text-decoration : line-through';
+}
+
+// const li =  document.createElement('li');
+//     li.appendChild(document.createTextNode(language));
+//     document.querySelector('.language').appendChild(li);
